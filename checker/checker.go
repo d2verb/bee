f0e5e92426d633fa2c481959f955418c4d7c2e4d
d2verb <licprog73@gmail.com>
2020-07-24 15:48:09 +0900

    feat: add checker test

---
+++ checker/checker.go
@@ -0,0 +1,140 @@
+package checker
+
+import (
+	"fmt"
+
+	"github.com/d2verb/bee/ast"
+)
+
+type Checker struct {
+	program   *ast.Program
+	functions map[string]int
+	variables map[string]struct{}
+	errors    []string
+}
+
+func New(program *ast.Program) *Checker {
+	c := &Checker{
+		program:   program,
+		functions: make(map[string]int),
+		errors:    []string{},
+	}
+	return c
+}
+
+func (c *Checker) Check() {
+	c.findFunctions()
+	for _, function := range c.program.Functions {
+		c.variables = make(map[string]struct{})
+
+		for _, parameter := range function.Parameters {
+			c.variables[parameter] = struct{}{}
+		}
+
+		c.checkBlockStatement(function.Body)
+	}
+}
+
+func (c *Checker) checkBlockStatement(node *ast.BlockStatement) {
+	for _, statement := range node.Statements {
+		c.checkStatement(statement)
+	}
+}
+
+func (c *Checker) checkStatement(node ast.Statement) {
+	switch node := node.(type) {
+	case *ast.BlockStatement:
+		c.checkBlockStatement(node)
+	case *ast.ReturnStatement:
+		c.checkExpression(node.Value)
+	case *ast.PutsStatement:
+		c.checkExpression(node.Value)
+	case *ast.IfStatement:
+		c.checkIfStatement(node)
+	case *ast.WhileStatement:
+		c.checkWhileStatement(node)
+	case *ast.ExpressionStatement:
+		c.checkExpression(node.Expression)
+	}
+}
+
+func (c *Checker) checkIfStatement(node *ast.IfStatement) {
+	c.checkExpression(node.Condition)
+	c.checkBlockStatement(node.Consequence)
+
+	if node.Alternative != nil {
+		c.checkBlockStatement(node.Alternative)
+	}
+}
+
+func (c *Checker) checkWhileStatement(node *ast.WhileStatement) {
+	c.checkExpression(node.Condition)
+	c.checkBlockStatement(node.Body)
+}
+
+func (c *Checker) checkExpression(node ast.Expression) {
+	switch node := node.(type) {
+	case *ast.InfixExpression:
+		if node.Operator == "=" {
+			c.checkExpression(node.Right)
+			c.registerVariable(node.Left.(*ast.Identifier).Value)
+		} else {
+			c.checkExpression(node.Left)
+			c.checkExpression(node.Right)
+		}
+	case *ast.PrefixExpression:
+		c.checkExpression(node.Right)
+	case *ast.CallExpression:
+		if !c.isFunctionExists(node.Function) {
+			msg := fmt.Sprintf("function '%s' is not defined", node.Function)
+			c.errors = append(c.errors, msg)
+			return
+		}
+		count, _ := c.functions[node.Function]
+		if len(node.Arguments) != count {
+			msg := fmt.Sprintf("the number of arguments for '%s' is not correct. expect=%d, got=%d",
+				node.Function,
+				count,
+				len(node.Arguments))
+			c.errors = append(c.errors, msg)
+			return
+		}
+		for _, argument := range node.Arguments {
+			c.checkExpression(argument)
+		}
+	case *ast.Identifier:
+		if !c.isVariableExists(node.Value) {
+			msg := fmt.Sprintf("variable '%s' is not defined", node.Value)
+			c.errors = append(c.errors, msg)
+		}
+	}
+}
+
+func (c *Checker) Errors() []string {
+	return c.errors
+}
+
+func (c *Checker) findFunctions() {
+	for _, function := range c.program.Functions {
+		c.functions[function.Name] = len(function.Parameters)
+	}
+}
+
+func (c *Checker) isFunctionExists(functionName string) bool {
+	_, ok := c.functions[functionName]
+	return ok
+}
+
+func (c *Checker) isVariableExists(variableName string) bool {
+	_, ok := c.variables[variableName]
+	return ok
+}
+
+func (c *Checker) parameterCount(functionName string) int {
+	count, _ := c.functions[functionName]
+	return count
+}
+
+func (c *Checker) registerVariable(variableName string) {
+	c.variables[variableName] = struct{}{}
+}
---
+++ checker/checker_test.go
@@ -0,0 +1,68 @@
+package checker
+
+import (
+	"testing"
+
+	"github.com/d2verb/bee/parser"
+
+	"github.com/d2verb/bee/lexer"
+)
+
+func TestChecker(t *testing.T) {
+	tests := []struct {
+		input  string
+		errors []string
+	}{
+		{
+			"fn main() {}",
+			[]string{},
+		},
+		{
+
+			"fn main() { main(1); main(1, 2) }",
+			[]string{
+				"the number of arguments for 'main' is not correct. expect=0, got=1",
+				"the number of arguments for 'main' is not correct. expect=0, got=2",
+			},
+		},
+		{
+
+			"fn main() { puts z; main(z); }",
+			[]string{
+				"variable 'z' is not defined",
+				"the number of arguments for 'main' is not correct. expect=0, got=1",
+			},
+		},
+		{
+
+			"fn main() { foo(1, 2); bar(1, 2); } fn foo(x, y) { return x + y; }",
+			[]string{
+				"function 'bar' is not defined",
+			},
+		},
+	}
+
+	for i, tt := range tests {
+		l := lexer.New(tt.input)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+
+		c := New(program)
+		c.Check()
+
+		errors := c.Errors()
+		if len(errors) != len(tt.errors) {
+			t.Errorf("[test-%d] the number of error message is not correct. expected=%d, got=%d",
+				i, len(tt.errors), len(errors))
+			break
+		}
+
+		for i := 0; i < len(errors); i++ {
+			if errors[i] != tt.errors[i] {
+				t.Errorf("[test-%d] error message is not correct. expected=%q, got=%q",
+					i, tt.errors[i], errors[i])
+			}
+		}
+	}
+}
